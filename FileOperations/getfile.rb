acbff4d67fa743ba2cf5e14fc8e3781d96c6c07d
Shailajarapolu95 <103415604+Shailajarapolu95@users.noreply.github.com>
2022-07-25 14:34:19 +0530

    Add files via upload

--- FileOperations/getfile.rb
+++ FileOperations/getfile.rb
@@ -1,1 +1,13 @@
-
+require 'humanize'
+class File
+    def demo
+      infile = "file.csv"
+      outfile = "output.csv"
+      File.open(outfile, 'w') do |out|
+        out << File.open(infile).read.gsub(/\d+/) { |r| r.to_i.humanize }
+      end  
+      puts "Output.csv file created and numbers chanded into words"
+   end
+end
+fileWrite = File.new
+fileWrite.demo
