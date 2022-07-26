3cff9ebc5172f5b17f6d2577c04b88725ba2ef59
Shailajarapolu95 <103415604+Shailajarapolu95@users.noreply.github.com>
2022-07-26 13:56:56 +0530

    Using Inheritance

--- using_inheritance.rb
+++ using_inheritance.rb
@@ -1,34 +1,32 @@
 class Add
-	def show
-		puts "You can Add and substract by using inheritance"
-	end
-	def Cal
-		puts "Enter a value to add "
-		a = gets.chomp.to_i
-		puts "Enter b value"
-		b = gets.chomp.to_i
-		c = a + b
-		puts "The Value Of A + B is = #{c}"
-	end
-end
+    def show
+       puts "You can Add and substract by using inheritance"
+    end
+    def Cal
+      puts "Enter a value to add "
+      a = gets.chomp.to_i
+      puts "Enter b value"
+      b = gets.chomp.to_i
+      c = a + b
+      puts "The Value Of A + B is = #{c}"
+    end
+  end
 
 class Sub < Add
-	def Cal1
-		puts "Enter a value to add "
-		x = read_input.to_i
-		puts "Enter b value"
-		y = read_input.to_i
-		z = subtract(x, y)
-		puts "The Value Of x - y is = #{z}"
-	end
-
-	def read_input
-		gets.chomp
-	end
-
-	def subtract(x, y)
-		x - y
-	end
+   def Cal1
+     puts "Enter a value to add "
+     x = read_input.to_i
+     puts "Enter b value"
+     y = read_input.to_i
+     z = subtract(x, y)
+     puts "The Value Of x - y is = #{z}"
+   end
+   def read_input
+      gets.chomp
+   end
+   def subtract(x, y)
+      x - y
+   end
 end
 
 ob1 = Sub.new
