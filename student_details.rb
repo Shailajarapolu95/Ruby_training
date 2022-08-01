f06e1622957f0334cd9971060397ee97788f712a
Shailajarapolu95 <103415604+Shailajarapolu95@users.noreply.github.com>
2022-08-01 15:57:40 +0530

    Add files via upload

--- student_details.rb
+++ student_details.rb
@@ -1,30 +1,35 @@
 class Student
- def set_student_details
-    puts "Enter Student name..."
-    @stud_name = gets.chomp
-    puts "Enter Roll number of a student..."
-    @roll_num = gets.chomp
-    puts "Enter Mark1..."
-    @mark1 = gets.chomp.to_i
-    puts "Enter Mark2..."
-    @mark2 = gets.chomp.to_i
-    puts "Enter Mark3..."
-    @mark3 = gets.chomp.to_i
-    @total_marks
+   attr_accessor :stud_name, :roll_num, :mark1, :mark2, :mark3
+   def initialize(stud_name, roll_num, mark1, mark2, mark3)
+      @stud_name = stud_name
+      @roll_num = roll_num
+      @mark1 = mark1
+      @mark2 = mark2
+      @mark3 = mark3
    end
    
-   def calculate_total
+   def calculate_total()
     @total_marks = @mark1 + @mark2 + @mark3
    end
 
-   def display_details
+   def display_details()
     puts "Roll Number : #{@roll_num}"
     puts "Student Name : #{@stud_name}"
     puts "Total Marks : #{@total_marks}"
     puts "Toatl Marks of #{@stud_name} is: #{@total_marks}"
    end
 end
-details = Student.new
-details.set_student_details
-details.calculate_total
-details.display_details
+puts "Enter Student name..."
+stud_name = gets.chomp
+puts "Enter Roll number of a student..."
+roll_num = gets.chomp
+puts "Enter Mark1..."
+mark1 = gets.chomp.to_i
+puts "Enter Mark2..."
+mark2 = gets.chomp.to_i
+puts "Enter Mark3..."
+mark3 = gets.chomp.to_i
+
+details = Student.new(stud_name, roll_num, mark1, mark2, mark3)
+details.calculate_total()
+details.display_details()
