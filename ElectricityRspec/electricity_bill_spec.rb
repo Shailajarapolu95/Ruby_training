a8ddc2df1854804c05121b17aee012e1b72ce2ad
Shailajarapolu95 <103415604+Shailajarapolu95@users.noreply.github.com>
2022-08-01 19:57:57 +0530

    Delete ElectricityRspec directory

--- ElectricityRspec/electricity_bill_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'electricity_bill'
-
-describe Electricity do
-  it "should test details of person" do
-    e = Electricity.new("shailaja", 234)
-    expect(e.calculate).to eq(2124)
-  end
-end
