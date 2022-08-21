c2d116cecfdde50022e443c902e705328c3765e7
Shailajarapolu95 <103415604+Shailajarapolu95@users.noreply.github.com>
2022-08-21 10:31:34 +0530

    Add files via upload

--- CricketGame/over.rb
+++ CricketGame/over.rb
@@ -1,20 +1,20 @@
 class Over
   def game_type
     puts "Enter type of the match"
-    $type = gets.chomp
-    if $type == "t20"
+    type = gets.chomp
+    if type == "t20"
       puts "You have selected T20"
       @total_balls = 5*6
-    elsif $type == "test"
+    elsif type == "test"
       puts "You have selected Test Match"
       @total_balls = 8*6
-    elsif $type == "one_day"
+    elsif type == "one_day"
       puts "You have selected One Day International"
       @total_balls = 10*6
     else
       puts "You didn't select a valid game type"
     end
-  puts "#{@total_balls}"
+  puts "Total balls is : #{@total_balls}"
   return @total_balls
  end
 end
