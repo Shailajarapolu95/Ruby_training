ee3e3b3b6805e59bdfbc4f58079b20fb9e230700
Shailajarapolu95 <103415604+Shailajarapolu95@users.noreply.github.com>
2022-08-22 09:06:46 +0530

    Add files via upload

--- CricketGame/RSpec/score_spec.rb
+++ CricketGame/RSpec/score_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative 'score.rb'
 
 describe Compare do
-  it "results must be integer" do
+  it "results must not be nil or false" do
     obj = Compare.new
     expect(obj.final).to be_truthy
   end
